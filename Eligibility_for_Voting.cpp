790c5ca88b311a8577b0577dccd0795c5f4f47a5
Akshit Bansal <155195875+akshitbansal2005@users.noreply.github.com>
2024-10-16 12:26:31 +0530

    Create Eligibility_for_Voting.cpp

---
+++ Eligibility_for_Voting.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age;
+    cout<<"enter the age for voting:"<<endl;
+    cin>>age;
+    try{
+        if(age<18){
+            throw age;
+        }
+    else{
+        cout<<"you are eligible for voting"<<endl;
+    }
+}
+catch(int age){
+    cout<<"\nERROR:You are not eligible for voting at:"<<age<<endl;
+}
+    return 0;
+}
+//Output
+/*enter the age for voting:
+14
+
+ERROR:You are not eligible for voting at:14*/
